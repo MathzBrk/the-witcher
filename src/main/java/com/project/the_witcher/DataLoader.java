236f597c4f0ea5c5a620b084ec8ecc708376bbaf
Matheus Borges <matheusvbborges@hotmail.com>
2024-10-24 15:20:18 -0300

    Add DataLoader class for saving and testing data

--- src/main/java/com/project/the_witcher/DataLoader.java
+++ src/main/java/com/project/the_witcher/DataLoader.java
@@ -18,12 +18,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Criar dados de exemplo
         Character character1 = new Character("Geralt de Rivia", "Masculino", CharacterCategory.WITCHER, "Caçador de monstros");
         Character character2 = new Character("Yennefer", "Feminino", CharacterCategory.SORCERESS, "Maga poderosa");
         Character character3 = new Character("Ciri", "Feminino", CharacterCategory.WITCHER, "A filha de surpresa de Geralt");
 
-        // Persistir os personagens no banco de dados
+        // Persistir os personagens n
         characterRepository.saveAll(Arrays.asList(character1, character2, character3));
     }
 }
